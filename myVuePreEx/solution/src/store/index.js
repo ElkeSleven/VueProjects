a2487d2f314248647bbbf2b6e83bdebf03d141b1
Elke Sleven <myprogrammersmail@gmail.com>
2022-06-13 22:41:09 +0200

    ..

---
+++ myVuePreEx/solution/src/store/index.js
@@ -0,0 +1,36 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    wallet: [
+      {code: 'BTC', name: 'Bitcoin', amountOwned: 1.2, logoPath: 'btc.jpg'},
+      {code: 'ETH', name: 'Ethereum', amountOwned: 16, logoPath: 'eth.png'},
+      {code: 'DOGE', name: 'DogeCoin', amountOwned: 42069, logoPath: 'doge.png'},
+      {code: 'LINK', name: 'Chainlink', amountOwned: 999, logoPath: 'link.png'},
+      {code: 'pony', name: 'microsoft', amountOwned: 2, logoPath: 'link.png'},
+    ],
+
+  },
+  getters: {
+  },
+  mutations: {
+    buy(state,payload)
+    {
+      let coinToBuy = state.wallet.find(coin =>
+      {return coin === payload.coin})
+
+      coinToBuy.amountOwned += payload.transactionAmount;
+    },
+    sell(state,payload) {
+      let coinToBuy = state.wallet.find(coin => {
+        return coin === payload.coin
+      })
+
+      coinToBuy.amountOwned -= payload.transactionAmount;
+    }
+    },
+  actions: {
+  },
+  modules: {
+  }
+})
